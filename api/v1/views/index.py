1b746045d63be3368c019deae4b1cf1e818db697
Nathan Machoka <natemachoka@gmail.com>
2024-08-03 05:54:45 -0700

    stats endpoint to retrieve object counts by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,17 +3,21 @@
 """
 This module defines the index route for the HBNB API.
 """
-from api.v1.views import app_views
+
 from flask import jsonify
+from api.v1.views import app_views
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
-def get_status():
-    """Returns the status of the API.
-    This endpoint returns a JSON object indicating that the API is running
-    and available.
-
-    Returns:
-        A JSON response with a single key-value pair: {"status": "OK"}
-    """
-    return jsonify({"status": "OK"})
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Retrieves the number of each object by type"""
+    object_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(object_counts)
